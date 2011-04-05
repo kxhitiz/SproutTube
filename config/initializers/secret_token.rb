f44597052255a71bf3d568776a9c43265204fccd
flame <kshitiz.gurung@sprout-technology.com>
2011-04-05 15:01:56 +0545

    Basic Framework Made

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+STube::Application.config.secret_token = '249d56b8699ef5c57cb7d0f1b58b100e06f811bee1297566752d38da044be3cfe4d2aefd15ea6feb0dd7c8e0ea50aab32a527169b514d500351bf85092b1d52a'
