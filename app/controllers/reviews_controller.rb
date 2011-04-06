84095d188acf328f68b885384074b6172b637f01
flame <kshitiz.gurung@sprout-technology.com>
2011-04-06 11:21:43 +0545

    Helper method created for calculating points for review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -12,11 +12,12 @@ class ReviewsController < ApplicationController
 
   def create
     @video = Video.find(params[:video_id])
-    @person = current_person.id
+    @person_id = current_person.id
     @review = @video.reviews.new(params[:review])
-    @review.person_id = @person
+    @review.person_id = @person_id
+    calculate_points(@review,@person_id)
     if @review.save
-      redirect_to videos_path, :notice => "Great! You Just Made a Review"
+      redirect_to video_path(@video), :notice => "Great! You Just Made a Review"
     else
       render :action => "new", :alert => "Oops! Unable to Post Review Right Now!"
     end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,18 @@
 module ApplicationHelper
 
+  def id2person(id)
+    @person_name = Person.find(id).name
+  end
+
+  def calculate_points(review,person_id)
+    # @review = review
+    #     @person = Person.find(params[:person_id])
+    #
+    #     @points = @person.points
+    #     if @review.content.length > 20
+    #       @points += 1
+    #     end
+  end
 
 end
 
